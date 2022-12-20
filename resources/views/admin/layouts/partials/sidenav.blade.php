440bdd3ece3ef2136169e42eb0377c903d3e0fff
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-12-20 23:23:44 +0800

    admin主樣板和子樣板文字設置

---
+++ resources/views/admin/layouts/partials/sidenav.blade.php
@@ -0,0 +1,31 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <a class="nav-link" href="{{ route('admin.index') }}">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    老師管理
+                </a>
+                <a class="nav-link" href="{{ route('admin.index') }}">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    課程管理
+                </a>
+                <a class="nav-link" href="{{ route('admin.index') }}">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    顧客資料
+                </a>
+            </div>
+
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            Start Bootstrap
+        </div>
+    </nav>
+</div>
