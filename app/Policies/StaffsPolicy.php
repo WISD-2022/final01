db780e873d75bec3aa2e7f89cfebf22cf32e2860
3A932045 <shadowy999@gmail.com>
2022-12-28 00:44:57 +0800

    修正staff資料表

--- app/Policies/StaffsPolicy.php
+++ app/Policies/StaffsPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Staff;
+use App\Models\Staffs;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class StaffPolicy
+class StaffsPolicy
 {
     use HandlesAuthorization;
 
@@ -25,10 +25,10 @@ class StaffPolicy
      * Determine whether the user can view the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Staff  $staff
+     * @param  \App\Models\Staffs  $staffs
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function view(User $user, Staff $staff)
+    public function view(User $user, Staffs $staffs)
     {
         //
     }
@@ -48,10 +48,10 @@ class StaffPolicy
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Staff  $staff
+     * @param  \App\Models\Staffs  $staffs
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function update(User $user, Staff $staff)
+    public function update(User $user, Staffs $staffs)
     {
         //
     }
@@ -60,10 +60,10 @@ class StaffPolicy
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Staff  $staff
+     * @param  \App\Models\Staffs  $staffs
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function delete(User $user, Staff $staff)
+    public function delete(User $user, Staffs $staffs)
     {
         //
     }
@@ -72,10 +72,10 @@ class StaffPolicy
      * Determine whether the user can restore the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Staff  $staff
+     * @param  \App\Models\Staffs  $staffs
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function restore(User $user, Staff $staff)
+    public function restore(User $user, Staffs $staffs)
     {
         //
     }
@@ -84,10 +84,10 @@ class StaffPolicy
      * Determine whether the user can permanently delete the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Staff  $staff
+     * @param  \App\Models\Staffs  $staffs
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function forceDelete(User $user, Staff $staff)
+    public function forceDelete(User $user, Staffs $staffs)
     {
         //
     }
