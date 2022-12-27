6216091e94d759cdd57394ad530e4a3ef1c3a3df
3A932045 <shadowy999@gmail.com>
2022-12-28 00:24:27 +0800

    建立課程管理頁面 part1

--- resources/views/admin/layouts/staffs/index.blade.php
+++ resources/views/admin/layouts/staffs/index.blade.php
@@ -1,12 +1,13 @@
 @extends('admin.master')
 @section('page-title', 'Dashboard')
-@section('page-content')
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">老師管理</h1>
-        <!--<ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">主控台</li>
-        </ol>-->
-        <!-- 今日預約的表格 -->
 
-    </div>
+@section('page-content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">老師管理</h1>
+    <!--<ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">主控台</li>
+    </ol>-->
+    <!-- 今日預約的表格 -->
+
+</div>
 @endsection
