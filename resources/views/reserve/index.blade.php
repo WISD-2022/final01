cd7c6aa63f6b0e73050bb0af91c220777a37f426
3A932045 <shadowy999@gmail.com>
2023-01-04 20:46:09 +0800

    瀏覽預約畫面

--- resources/views/reserve/index.blade.php
+++ resources/views/reserve/index.blade.php
@@ -3,50 +3,45 @@
 @section('page-title', '預約表')
 
 @section('page-content')
-    <p>pppppppppp</p>
-    <!--   <div class="container-fluid px-4">
-        <h1 class="mt-4">預約課程</h1>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">課程管理</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item active">課程一覽表</li>
         </ol>
-        <div class="alert alert-success alert-dismissible" role="alert" id="liveAlert">
-            <strong>完成！</strong> 預約成功
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <a class="btn btn-success btn-sm" href="{{ route('reserve.create') }}">新增</a>
-        </div>
         <table class="table">
             <thead>
             <tr>
-                <th scope="col">#</th>-->
-                <!--<th scope="col">標題</th>
-                <th scope="col" style="text-align: left">標題</th>
-                <th scope="col" style="text-align: right">精選?</th>
-                <th scope="col">功能</th>
+                <!--<th scope="col">標題</th>-->
+                <th scope="col" style="text-align: left;width: 10%">ID</th>
+                <th scope="col" style="text-align: left;width: 15%" >課程名稱</th>
+                <th scope="col" style="text-align: left;width: 15%">老師</th>
+                <th scope="col" style="text-align: right;width: 15%" >日期</th>
+                <th scope="col" style="text-align: right;width: 10%">金額</th>
+                <th scope="col" style="text-align: right;width: 10%">功能</th>
             </tr>
-            </thead>-->
-            <!-- <tbody>
-             $posts資料表取單一個*
-            @foreach($posts as $post)
+            </thead>
+            <tbody>
+            @foreach($reserves as $reserves)
                 <tr>
-                    <td style="text-align: right">{{$post->id}}</td>
-                    <td>{{$post->title}}</td>
-                    <td style="text-align: right">{{($post->is_feature)? 'v' : 'x'}}</td>
-                    <td>
-                        <a class="btn btn-sm btn-primary" href="{{route('admin.posts.edit', $post->id)}}">編輯</a>
-                        /
-                        <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST" style="display: inline-block">
+                    <!--<th scope="col">標題</th>-->
+                    <td style="text-align: left">{{$reserves->id}}</td>
+                    <td style="text-align: left"></td>
+                    <td style="text-align: left;width: 10%"></td>
+                    <td style="text-align: right;width: 10%">{{$reserves->date}},{{$reserves->str_time}}-{{$reserves->end_time}}</td>
+                    <td style="text-align: right;width: 10%">{{$reserves->pay}}</td>
+                    <td style="text-align: right;width: 10%">
+                        <a class="btn btn-secondary" href="">修改</a>
+                        <form action="" method="POST">
+                            @method('DELETE')
+                            @csrf
                             {{method_field('DELETE')}}
                             {{csrf_field()}}
-                            <button class="btn btn-sm btn-danger" type="submit">刪除</button>
+                            <button class="btn btn-danger">刪除</button>
                         </form>
-
                     </td>
-
                 </tr>
             @endforeach
             </tbody>
         </table>
-    </div>-->
+    </div>
 @endsection
