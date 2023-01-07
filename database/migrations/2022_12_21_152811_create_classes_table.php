19d9594b7876a30a53be4f1d92571b5256dbb01c
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2023-01-07 21:37:56 +0800

    migration修正

--- database/migrations/2022_12_21_152811_create_classes_table.php
+++ database/migrations/2022_12_21_152811_create_classes_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->text('intro');
             $table->integer('amount');
             $table->time('time');
-            $table->int('ismember');
+            //$table->int('ismember');
             $table->timestamps();
         });
     }
